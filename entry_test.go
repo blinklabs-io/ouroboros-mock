58329ed4d28b3e9949d5a6c49c8f793ad364d321
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-06 14:57:27 -0400

    feat: add Leios mock conversations
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ entry_test.go
@@ -0,0 +1,45 @@
+// Copyright 2026 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+package ouroboros_mock_test
+
+import (
+	"testing"
+
+	"github.com/blinklabs-io/gouroboros/protocol/leiosfetch"
+	"github.com/blinklabs-io/gouroboros/protocol/leiosnotify"
+	"github.com/blinklabs-io/gouroboros/protocol/leiosvotes"
+	mock "github.com/blinklabs-io/ouroboros-mock"
+)
+
+func TestLeiosConversationBuilders(t *testing.T) {
+	fetch := mock.NewConversationEntryLeiosFetchRequest(leiosfetch.NewMsgDone())
+	if fetch.ProtocolId != leiosfetch.ProtocolId || fetch.MsgFromCborFunc == nil {
+		t.Fatalf("unexpected Leios fetch entry: %#v", fetch)
+	}
+
+	notify := mock.NewConversationEntryLeiosNotifyRequest(leiosnotify.NewMsgDone())
+	if notify.ProtocolId != leiosnotify.ProtocolId || notify.MsgFromCborFunc == nil {
+		t.Fatalf("unexpected Leios notify entry: %#v", notify)
+	}
+
+	votes := mock.NewConversationEntryLeiosVotesRequest(leiosvotes.NewMsgDone())
+	if votes.ProtocolId != leiosvotes.ProtocolId || votes.MsgFromCborFunc == nil {
+		t.Fatalf("unexpected Leios votes entry: %#v", votes)
+	}
+
+	for name, conversation := range map[string][]mock.ConversationEntry{
+		"fetch":  mock.ConversationLeiosFetch,
+		"notify": mock.ConversationLeiosNotify,
+		"votes":  mock.ConversationLeiosVotes,
+	} {
+		if len(conversation) != 3 {
+			t.Errorf("%s conversation length = %d, want 3", name, len(conversation))
+		}
+	}
+}
